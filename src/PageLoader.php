dc0f2952e0cc9249d7574a352f713f2daf202254
free-donut <yula@freedonut.space>
2021-10-24 19:39:53 +0300

    Refactoring PageLoader

--- src/PageLoader.php
+++ src/PageLoader.php
@@ -14,16 +14,17 @@ class PageLoader
             $filesDirectory = $this->getFilesDirectoryName($url);
             $pathToFiles = $path . DIRECTORY_SEPARATOR . $filesDirectory;
             $this->createDir($pathToFiles);
-            foreach ($images as $image) {
+            foreach ($images as $key => $image) {
                 $imageUrl = $image->src;
                 if ($this->isLocalLink($imageUrl, $url)) {
                     $imageName = $this->getImageName($url, $imageUrl);
                     $newImageLink = $filesDirectory . DIRECTORY_SEPARATOR . $imageName;
-                    $content = str_replace($imageUrl, $newImageLink, $content);
+                    $document->find('img')[$key]->src = $newImageLink;
                     $imagePath = $path . DIRECTORY_SEPARATOR . $newImageLink;
                     $client->request('GET', $imageUrl, ['sink' => $imagePath]);
                 }
             }
+            $content = $document->format()->html();
         }
         $this->createDir($path);
         $pageFileName = $this->getPageFileName($url);
@@ -34,9 +35,9 @@ class PageLoader
 
     private function getPageFileName($url): string
     {
-        $formattedUrl = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
+        $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
         $formattedPath = preg_replace('/\W/', '-', parse_url($url, PHP_URL_PATH));
-        return $formattedUrl . $formattedPath . '.html';
+        return $formattedHost . $formattedPath . '.html';
     }
 
     private function createDir($path): self
@@ -49,19 +50,19 @@ class PageLoader
 
     private function getFilesDirectoryName(string $url): string
     {
-        $formattedUrl = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
+        $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
         $formattedPath = preg_replace('/\W/', '-', parse_url($url, PHP_URL_PATH));
-        return $formattedUrl . $formattedPath . '_files';
+        return $formattedHost . $formattedPath . '_files';
     }
     private function getImageName($url, $imageUrl): string
     {
-        $formattedUrl = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
+        $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
         if (filter_var($imageUrl, FILTER_VALIDATE_URL)) {
             $formattedPath = str_replace('/', '-', parse_url($imageUrl, PHP_URL_PATH));
         } else {
             $formattedPath = str_replace('/', '-', $imageUrl);
         }
-        return $formattedUrl . $formattedPath;
+        return $formattedHost . $formattedPath;
     }
 
     private function isLocalLink(string $imageUrl, string $url): bool
