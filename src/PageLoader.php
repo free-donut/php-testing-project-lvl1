112b67f06f627710eaf33a4e9c78f4fd0ae6f047
free-donut <yula@freedonut.space>
2021-10-24 17:27:03 +0300

    Added image loading

--- src/PageLoader.php
+++ src/PageLoader.php
@@ -2,19 +2,34 @@
 
 namespace Hexlet\Code;
 
+use DiDom\Document;
+
 class PageLoader
 {
     public function downloadPage($url, $path, $client): string
     {
         $content = $client->get($url)->getBody()->getContents();
+        $document = new Document($content);
+        if (count($images = $document->find('img')) > 0) {
+            $filesDirectory = $this->getFilesDirectoryName($url);
+            $pathToFiles = $path . DIRECTORY_SEPARATOR . $filesDirectory;
+            $this->createDir($pathToFiles);
+            foreach ($images as $image) {
+                $imageName = $this->getImageName($url, $image->src);
+                $newImageLink = $filesDirectory . DIRECTORY_SEPARATOR . $imageName;
+                $content = str_replace($image->src, $newImageLink, $content);
+                $imagePath = $path . DIRECTORY_SEPARATOR . $newImageLink;
+                $client->request('GET', $image->src, ['sink' => $imagePath]);
+            }
+        }
         $this->createDir($path);
-        $fileName = $this->getFileName($url);
-        $fullFilePath = $path . DIRECTORY_SEPARATOR . $this->getFileName($url);
+        $pageFileName = $this->getPageFileName($url);
+        $fullFilePath = $path . DIRECTORY_SEPARATOR . $pageFileName;
         file_put_contents($fullFilePath, $content);
         return $fullFilePath;
     }
 
-    private function getFileName($url): string
+    private function getPageFileName($url): string
     {
         $formattedUrl = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
         $formattedPath = preg_replace('/\W/', '-', parse_url($url, PHP_URL_PATH));
@@ -27,5 +42,18 @@ class PageLoader
             mkdir($path, 0777, true);
         }
         return $this;
+    }
+
+    private function getFilesDirectoryName(string $url): string
+    {
+        $formattedUrl = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
+        $formattedPath = preg_replace('/\W/', '-', parse_url($url, PHP_URL_PATH));
+        return $formattedUrl . $formattedPath . '_files';
+    }
+    private function getImageName($url, $path): string
+    {
+        $formattedUrl = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
+        $formattedPath = str_replace('/', '-', $path);
+        return $formattedUrl . $formattedPath;
     }
 }
