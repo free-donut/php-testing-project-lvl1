32bedfb64a4c997498bb884fd920ba66f4957ce6
free-donut <yula@freedonut.space>
2021-11-14 13:45:03 +0300

    Added downloading of other resources

--- src/PageLoader.php
+++ src/PageLoader.php
@@ -10,25 +10,51 @@ class PageLoader
     {
         $content = $client->get($url)->getBody()->getContents();
         $document = new Document($content);
-        if (count($images = $document->find('img')) > 0) {
-            $filesDirectory = $this->getFilesDirectoryName($url);
-            $pathToFiles = $path . DIRECTORY_SEPARATOR . $filesDirectory;
-            $this->createDir($pathToFiles);
-            foreach ($images as $key => $image) {
-                $imageUrl = $image->src;
-                if ($this->isLocalLink($imageUrl, $url)) {
-                    $imageName = $this->getImageName($url, $imageUrl);
-                    $newImageLink = $filesDirectory . DIRECTORY_SEPARATOR . $imageName;
-                    $document->find('img')[$key]->src = $newImageLink;
-                    $imagePath = $path . DIRECTORY_SEPARATOR . $newImageLink;
-                    $client->request('GET', $imageUrl, ['sink' => $imagePath]);
+        $filesDirectory = $this->getFilesDirectoryName($url);
+        $pathToFiles = "{$path}/{$filesDirectory}";
+
+        $images = $document->find('img');
+        foreach ($images as $key => $image) {
+            $imageUrl = $image->src;
+            if ($this->isLocalResource($imageUrl, $url)) {
+                $this->createDir($pathToFiles);
+                $newImageLink = $this->getResourceLink($imageUrl, $url, $filesDirectory);
+                $document->find('img')[$key]->src = $newImageLink;
+                $imagePath = "{$path}/{$newImageLink}";
+                $client->request('GET', $imageUrl, ['sink' => $imagePath]);
+            }
+        }
+
+        $links = $document->find('link');
+        foreach ($links as $key => $link) {
+            $linkUrl = $link->href;
+            if ($this->isLocalResource($linkUrl, $url)) {
+                $this->createDir($pathToFiles);
+                $isCanonicalLink = ($link->rel === 'canonical');
+                $newScriptLink = $this->getResourceLink($linkUrl, $url, $filesDirectory, $isCanonicalLink);
+                $document->find('link')[$key]->href = $newScriptLink;
+                $linkPath = "{$path}/{$newScriptLink}";
+                $client->request('GET', $linkUrl, ['sink' => $linkPath]);
+            }
+        }
+
+        $scripts = $document->find('script');
+        foreach ($scripts as $key => $script) {
+            if (null !== ($scriptUrl = $script->src)) {
+                if ($this->isLocalResource($scriptUrl, $url)) {
+                    $this->createDir($pathToFiles);
+                    $newScriptLink = $this->getResourceLink($scriptUrl, $url, $filesDirectory);
+                    $document->find('script')[$key]->src = $newScriptLink;
+                    $scriptPath = "{$path}/{$newScriptLink}";
+                    $client->request('GET', $scriptUrl, ['sink' => $scriptPath]);
                 }
             }
-            $content = $document->format()->html();
         }
+
+        $content = $document->format()->html();
         $this->createDir($path);
         $pageFileName = $this->getPageFileName($url);
-        $fullFilePath = $path . DIRECTORY_SEPARATOR . $pageFileName;
+        $fullFilePath = "{$path}/{$pageFileName}";
         file_put_contents($fullFilePath, $content);
         return $fullFilePath;
     }
@@ -37,7 +63,7 @@ class PageLoader
     {
         $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
         $formattedPath = preg_replace('/\W/', '-', parse_url($url, PHP_URL_PATH));
-        return $formattedHost . $formattedPath . '.html';
+        return "{$formattedHost}{$formattedPath}.html";
     }
 
     private function createDir($path): self
@@ -52,24 +78,30 @@ class PageLoader
     {
         $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
         $formattedPath = preg_replace('/\W/', '-', parse_url($url, PHP_URL_PATH));
-        return $formattedHost . $formattedPath . '_files';
-    }
-    private function getImageName($url, $imageUrl): string
-    {
-        $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
-        if (filter_var($imageUrl, FILTER_VALIDATE_URL)) {
-            $formattedPath = str_replace('/', '-', parse_url($imageUrl, PHP_URL_PATH));
-        } else {
-            $formattedPath = str_replace('/', '-', $imageUrl);
-        }
-        return $formattedHost . $formattedPath;
+        return "{$formattedHost}{$formattedPath}_files";
     }
 
-    private function isLocalLink(string $imageUrl, string $url): bool
+    private function getResourceLink(string $resourceUrl, string $url, string $filesDirectory, $isCanonicalLink = false): string
     {
-        if (filter_var($imageUrl, FILTER_VALIDATE_URL) === false) {
+        if ($isCanonicalLink) {
+            $pageFileName = $this->getPageFileName($url);
+            return "{$filesDirectory}/{$pageFileName}";
+        }
+        $formattedHost = preg_replace('/\W/', '-', parse_url($url, PHP_URL_HOST));
+        if (filter_var($resourceUrl, FILTER_VALIDATE_URL)) {
+            $formattedPath = str_replace('/', '-', parse_url($resourceUrl, PHP_URL_PATH));
+        } else {
+            $formattedPath = str_replace('/', '-', $resourceUrl);
+        }
+
+        return "{$filesDirectory}/{$formattedHost}{$formattedPath}";
+    }
+
+    private function isLocalResource(string $resourceUrl, string $url): bool
+    {
+        if (filter_var($resourceUrl, FILTER_VALIDATE_URL) === false) {
             return true;
         }
-        return parse_url($imageUrl, PHP_URL_HOST) === parse_url($url, PHP_URL_HOST);
+        return parse_url($resourceUrl, PHP_URL_HOST) === parse_url($url, PHP_URL_HOST);
     }
 }
