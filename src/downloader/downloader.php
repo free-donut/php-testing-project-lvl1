709c34cc9bf6c06ebd4fbade8ef625e1a6a8041e
free-donut <yula@freedonut.space>
2022-02-01 19:09:38 +0300

    Add downloadPage function

---
+++ src/downloader/downloader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Downloader\Downloader;
+
+use Downloader\Downloader;
+use Monolog\Logger;
+
+function downloadPage($url, $output, $clientClass)
+{
+	$client = new $clientClass();
+	$logger = new Logger('LOGGER');
+	try {
+	    $pageLoader = new Downloader($logger);
+	    $path = $pageLoader->downloadPage($url, $output, $client);
+	    print_r('Page was successfully downloaded into ');
+	    print_r($path);
+	    print_r(PHP_EOL);
+	} catch (\Exception $e) {
+	    fwrite(STDERR, sprintf('Application terminated with an error: "%s"%s', $e->getMessage(), PHP_EOL));
+	    exit($e->getCode());
+	}
+	
+}
