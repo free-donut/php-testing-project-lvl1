df91295d123948407216580fadd56a84d56ba5d7
free-donut <yula@freedonut.space>
2022-02-01 19:43:11 +0300

    Linter

--- src/downloader/downloader.php
+++ src/downloader/downloader.php
@@ -7,17 +7,16 @@ use Monolog\Logger;
 
 function downloadPage($url, $output, $clientClass)
 {
-	$client = new $clientClass();
-	$logger = new Logger('LOGGER');
-	try {
-	    $pageLoader = new Downloader($logger);
-	    $path = $pageLoader->downloadPage($url, $output, $client);
-	    print_r('Page was successfully downloaded into ');
-	    print_r($path);
-	    print_r(PHP_EOL);
-	} catch (\Exception $e) {
-	    fwrite(STDERR, sprintf('Application terminated with an error: "%s"%s', $e->getMessage(), PHP_EOL));
-	    exit($e->getCode());
-	}
-	
+    $client = new $clientClass();
+    $logger = new Logger('LOGGER');
+    try {
+        $pageLoader = new Downloader($logger);
+        $path = $pageLoader->downloadPage($url, $output, $client);
+        print_r('Page was successfully downloaded into ');
+        print_r($path);
+        print_r(PHP_EOL);
+    } catch (\Exception $e) {
+        fwrite(STDERR, sprintf('Application terminated with an error: "%s"%s', $e->getMessage(), PHP_EOL));
+        exit($e->getCode());
+    }
 }
